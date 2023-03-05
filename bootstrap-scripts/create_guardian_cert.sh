3acb45c95a864e74a9e4dc7f0154b37852cdacf4
hpaskalev <h.paskalev@gmail.com>
2023-03-05 15:48:19 +0200

    Fixed creation of guardian

--- bootstrap-scripts/create_guardian_cert.sh
+++ bootstrap-scripts/create_guardian_cert.sh
@@ -25,5 +25,6 @@ done
 
 node_dir="heimdall-$node"
 node_base_port=$(($federation_base_port+$node*10))
-$FM_BIN_DIR/distributedgen create-cert --out-dir "$TENANTS_DIR/$federation_id/$node_dir" \
-	--address "$HOST_ADDR" --base-port "$node_base_port" --name "$name" --password "$secret"
+node_api_port=$(($node_base_port + 1))
+(FM_PASSWORD="$secret" $FM_BIN_DIR/distributedgen create-cert --out-dir "$TENANTS_DIR/$federation_id/$node_dir" \
+	--p2p-url "ws://$HOST_ADDR:$node_base_port" --api-url "ws://$HOST_ADDR:$node_api_port" --name "$name")
